eecc6daa7c0c2eb7f2ada84c53f7482ac62606bd
alamtaab <167731823+alamtaab@users.noreply.github.com>
2025-10-28 17:56:49 -0700

    Update SerialApp.java

--- software/triad_gui/src/SerialApp.java
+++ software/triad_gui/src/SerialApp.java
@@ -5,7 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.Scanner;
 
-public class SerialApp {
+public class SerialApp extends JFrame {
 
 
+    public SerialApp() {
+        super("Triad GUI");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700, 500);
+        setLayout(new BorderLayout());
+    }
 }
