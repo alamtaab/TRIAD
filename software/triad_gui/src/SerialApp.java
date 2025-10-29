270f9286051bb263a805d544addb6b5ff68714f3
alamtaab <167731823+alamtaab@users.noreply.github.com>
2025-10-28 17:53:15 -0700

    Create SerialApp.java

---
+++ software/triad_gui/src/SerialApp.java
@@ -0,0 +1,11 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Scanner;
+
+public class SerialApp {
+
+
+}
