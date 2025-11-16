0bce2a0940ff1e00e4882f0b07dbfc67f194c7d2
alamtaab <167731823+alamtaab@users.noreply.github.com>
2025-11-16 02:06:22 -0800

    define config constants

--- firmware/c-c++/tdoa_onset_logic.c
+++ firmware/c-c++/tdoa_onset_logic.c
@@ -4,6 +4,17 @@
 #include <stdint.h>
 #include <time.h>
 
+// config const.
+
+// samples captured by dma pre-processing
+#define BUFFER_SIZE 4096
+
+// 3 microphones
+#define NUM_CHANNELS 3
+
+// sample rate per microphone
+#define SAMPLE_RATE_PER_CHANNEL 48000.0f // Hz
+
 
 int main() {
 
